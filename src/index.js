1ecdd9e7122d9aee3823e2c827d0e361d92868d7
annomao <omaoannkerubo@gmail.com>
2022-04-12 19:58:36 +0300

    add functionality to add and display task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.forms[0]
+  let text = form.querySelector("#new-task-description")
+
+  form.addEventListener("submit",(event) =>{
+
+    event.preventDefault()
+
+    //retrieve ul element
+    let ul = document.querySelector("#tasks")
+
+    //create li and button elements
+    let li = document.createElement("li")
+    let deleteBtn = document.createElement("button")
+
+    //delete task functionality
+    deleteBtn.addEventListener("click", deleteTask)
+
+    //set text content value for the button and list element
+    li.textContent = text.value
+    deleteBtn.textContent = "X"
+
+    //style delete button
+    deleteBtn.style.margin = "5px"
+    deleteBtn.style.color = "red"
+
+    //append button to li element then append list to ul element
+    li.appendChild(deleteBtn)
+    ul.appendChild(li)
+
+    //set input field to empty after submit
+    text.value = ""
+  })
+
+  function deleteTask(e){
+    e.target.parentNode.remove()
+  }
 });
