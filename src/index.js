88e27ca412b3fccff582fe420d015b49a1f31f7d
annomao <omaoannkerubo@gmail.com>
2022-04-12 20:21:25 +0300

    add priority check that determines color

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let form = document.forms[0]
   let text = form.querySelector("#new-task-description")
+  let priority = form.querySelector("#priority")
 
   form.addEventListener("submit",(event) =>{
 
     event.preventDefault()
 
-    //retrieve ul element
     let ul = document.querySelector("#tasks")
 
-    //create li and button elements
     let li = document.createElement("li")
     let deleteBtn = document.createElement("button")
 
@@ -18,8 +17,21 @@ document.addEventListener("DOMContentLoaded", () => {
     deleteBtn.addEventListener("click", deleteTask)
 
     //set text content value for the button and list element
+
     li.textContent = text.value
     deleteBtn.textContent = "X"
+
+    //set color based on priority
+    let chosenColor = priority.value
+    if (chosenColor==="red"){
+      li.style.color = "red"
+    }
+    else if(chosenColor==="yellow"){
+      li.style.color = "yellow"
+    }
+    else{
+      li.style.color = "green"
+    }
 
     //style delete button
     deleteBtn.style.margin = "5px"
@@ -30,10 +42,11 @@ document.addEventListener("DOMContentLoaded", () => {
     ul.appendChild(li)
 
     //set input field to empty after submit
-    text.value = ""
+    form.reset()
   })
 
   function deleteTask(e){
     e.target.parentNode.remove()
   }
+
 });
